7e47dc8893c5314640f0547e3646a0410a0b18bf
alessandro ladu <ale1a@libero.it>
2022-12-11 16:20:28 +0000

    use HostBinding to avoid using Renderer

--- src/app/better-highlight/better-highlight.directive.ts
+++ src/app/better-highlight/better-highlight.directive.ts
@@ -4,21 +4,25 @@ import {
   OnInit,
   ElementRef,
   HostListener,
+  HostBinding,
 } from '@angular/core';
 
 @Directive({
   selector: '[appBetterHighlight]',
 })
 export class BetterHighlightDirective implements OnInit {
+  @HostBinding('style.color') color: string = 'white';
   constructor(private elRef: ElementRef, private renderer: Renderer2) {}
   ngOnInit() {
     this.renderer.setStyle(this.elRef.nativeElement, 'backgroundColor', 'blue');
-    this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
   }
   @HostListener('mouseenter') mouseover(eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, 'color', 'red');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'color', 'red');
+    this.color = 'red';
   }
   @HostListener('mouseleave') mouseleave(eventData: Event) {
-    this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
+    // this.renderer.setStyle(this.elRef.nativeElement, 'color', 'white');
+    this.color = 'white';
   }
 }
